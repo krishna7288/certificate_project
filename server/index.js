d1982562632c58a4cbc46a20878874aa169f80d2
Krishna <89905895+krishna7288@users.noreply.github.com>
2023-11-28 12:17:14 +0530

    commit certificate

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const connectDB = require('./model/connection');
 const routes = require('./routes/route');
 const path = require('path');
 
-app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -15,13 +14,11 @@ app.set('views', __dirname + '/views');
 // app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
-
+app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', routes);
-
 
 connectDB(); // Establish MongoDB connection
 
--- server/model/connection.js
+++ server/model/connection.js
@@ -1,19 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
-// Load environment variables from .env file
-require('dotenv').config();
-
 const connectDB = () => {
-  const mongodbUri = process.env.MONGODB_URI;
-  if (!mongodbUri) {
-    console.error('MONGODB_URI environment variable is not defined.');
-    return;
-  }
-
   mongoose
-    .connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
+    .connect("mongodb+srv://dev:xjvcptTN8CZ0G98q@cluster0.ricqecm.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {     
       console.log('Connected to MongoDB');
     })
     .catch((error) => {
